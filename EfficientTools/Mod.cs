316a023a22591e573f99f79a937f67559388388b
daniele <daniele.lopresti97@gmail.com>
2022-04-18 16:12:44 +0200

    added EfficientTools mod

---
+++ EfficientTools/Mod.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using Assets.Scripts.Objects.Items;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace EfficientTools
+{
+	// Token: 0x02000002 RID: 2
+	[BepInPlugin("resurak.EfficientTools", "Efficient Tools", "0.1")]
+	public class Mod : BaseUnityPlugin
+	{
+		public const string pluginGuid = "resurak.EfficientTools";
+		public const string pluginName = "Efficient Tools";
+		public const string pluginVersion = "0.1";
+
+		internal static ManualLogSource Log;
+
+		public void Awake()
+		{
+			// Creating logger
+			Log = base.Logger;
+			Log.LogInfo("Loading mod");
+
+			// Creating harmony instance
+			Harmony harmony = new Harmony(pluginGuid);
+
+			// Getting original method from game and patched method
+			var PowerTool_OnUseItem = typeof(PowerTool).GetMethod(nameof(PowerTool.OnUseItem));
+			var PowerTool_OnUseItem_Prefix = typeof(Patches).GetMethod(nameof(Patches.PowerTool_OnUseItem_Prefix));
+			var PowerTool_OnUseItem_Postfix = typeof(Patches).GetMethod(nameof(Patches.PowerTool_OnUseItem_Postfix));
+
+			var MiningDrill_OnMinedOre = typeof(MiningDrill).GetMethod(nameof(MiningDrill.OnMinedOre));
+			var MiningDrill_OnMinedOre_Prefix = typeof(Patches).GetMethod(nameof(Patches.MiningDrill_OnMinedOre_Prefix));
+			var MiningDrill_OnMinedOre_Postfix = typeof(Patches).GetMethod(nameof(Patches.MiningDrill_OnMinedOre_Postfix));
+
+			var ArcWeldingTool_OnPowerTick = typeof(ArcWeldingTool).GetMethod(nameof(ArcWeldingTool.OnPowerTick));
+			var ArcWeldingTool_OnPowerTick_Prefix = typeof(Patches).GetMethod(nameof(Patches.ArcWeldingTool_OnPowerTick_Prefix));
+
+			// Patching 
+			Log.LogInfo("Patching PowerTool.OnUseItem");
+			harmony.Patch(PowerTool_OnUseItem, new HarmonyMethod(PowerTool_OnUseItem_Prefix), new HarmonyMethod(PowerTool_OnUseItem_Postfix));
+
+			Log.LogInfo("Patching MiningDrill.OnMinedOre");
+			harmony.Patch(MiningDrill_OnMinedOre, new HarmonyMethod(MiningDrill_OnMinedOre_Prefix), new HarmonyMethod(MiningDrill_OnMinedOre_Postfix));
+
+			Log.LogInfo("Patching ArcWeldingTool.OnPowerTick");
+			harmony.Patch(ArcWeldingTool_OnPowerTick, prefix: new HarmonyMethod(ArcWeldingTool_OnPowerTick_Prefix));
+
+			Log.LogInfo("Loaded correclty");
+		}
+	}
+}
---
+++ EfficientTools/Patches.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Assets.Scripts.Objects;
+using Assets.Scripts.Objects.Items;
+
+namespace EfficientTools
+{
+	public class Patches
+	{
+		protected internal static float PowerSub = 360f;
+
+		protected internal static float PowerTool_StoredPower = 0f;
+        protected internal static float MiningDrill_StoredPower = 0f;
+        protected internal static float ArcWeldingTool_UsedPassivePower = 720f;
+
+		public static void PowerTool_OnUseItem_Prefix(float quantity, Thing onUseThing, PowerTool __instance)
+        {
+			PowerTool_StoredPower = __instance.Battery.PowerStored;
+        }
+
+		public static void PowerTool_OnUseItem_Postfix(float quantity, Thing onUseThing, PowerTool __instance)
+        {
+			__instance.Battery.PowerStored = PowerTool_StoredPower;
+			__instance.Battery.PowerStored -= quantity >= PowerSub ? PowerSub : quantity;
+        }
+
+        public static void MiningDrill_OnMinedOre_Prefix(MiningDrill __instance, Ore oreMined)
+        {
+            MiningDrill_StoredPower = __instance.Battery.PowerStored;
+        }
+
+        public static void MiningDrill_OnMinedOre_Postfix(MiningDrill __instance, Ore oreMined)
+        {
+            __instance.Battery.PowerStored = MiningDrill_StoredPower;
+            __instance.Battery.PowerStored -= PowerSub;
+        }
+
+        public static void ArcWeldingTool_OnPowerTick_Prefix(ArcWeldingTool __instance)
+        {
+            __instance.UsedPowerPassive = ArcWeldingTool_UsedPassivePower;
+        }
+    }
+}
