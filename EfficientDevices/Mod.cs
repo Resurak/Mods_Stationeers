a0be75bc31ceb6aa70db2cfb5b6e24a351fa0839
daniele <daniele.lopresti97@gmail.com>
2022-04-24 21:44:54 +0200

    added PipeHeater power draw, heat power added optional PipeHeater advanced mode

--- EfficientDevices/Mod.cs
+++ EfficientDevices/Mod.cs
@@ -23,8 +23,18 @@ namespace EfficientDevices
         public static ConfigFloat AirConditioner_MinPower;
         public static ConfigFloat AirConditioner_MaxPower;
 
-        public static ConfigFloat TurboPumpGas_MinPower;
-        public static ConfigFloat TurboPumpGas_MaxPower;
+        public static ConfigFloat TurboPump_MinPower;
+        public static ConfigFloat TurboPump_MaxPower;
+
+        public static ConfigFloat PipeHeater_UsedPower;
+        public static ConfigFloat PipeHeater_HeatPower;
+
+        public static ConfigBool PipeHeater_Advanced;
+        public static ConfigBool PipeHeater_OnOffOnTemp;
+        public static ConfigBool PipeHeater_AllowCooling;
+        public static ConfigBool PipeHeater_AutoHeatPower;
+
+        public static ConfigFloat PipeHeater_DesiredTemp;
 
         // Plugin logger
         internal static ManualLogSource Log;
@@ -61,8 +71,17 @@ namespace EfficientDevices
             AirConditioner_MinPower = ConfigHandler.LoadFloat("AirConditioner", "MinPower", "Minimum power draw", 100f);
             AirConditioner_MaxPower = ConfigHandler.LoadFloat("AirConditioner", "MaxPower", "Maximum power draw", 1500f);
 
-            TurboPumpGas_MinPower = ConfigHandler.LoadFloat("TurboPumpGas", "MinPower", "Minimum power draw", 50f);
-            TurboPumpGas_MaxPower = ConfigHandler.LoadFloat("TurboPumpGas", "MaxPower", "Maximum power draw", 200f);
+            TurboPump_MinPower = ConfigHandler.LoadFloat("TurboPumpGas", "MinPower", "Minimum power draw", 50f);
+            TurboPump_MaxPower = ConfigHandler.LoadFloat("TurboPumpGas", "MaxPower", "Maximum power draw", 200f);
+
+            PipeHeater_UsedPower = ConfigHandler.LoadFloat("PipeHeater", "UsedPower", "Power draw", 1000f);
+            PipeHeater_HeatPower = ConfigHandler.LoadFloat("PipeHeater", "HeatPower", "Heat power (Joules per tick)", 1000f);
+
+            PipeHeater_Advanced = ConfigHandler.LoadBool("PipeHeater.Advanced", "Enabled", "Enables the advanced mode", false);
+            PipeHeater_OnOffOnTemp = ConfigHandler.LoadBool("PipeHeater.Advanced", "OnOffOnTemp", "Power on/off the heater when the temp is below the setted temp", true);
+            PipeHeater_AutoHeatPower = ConfigHandler.LoadBool("PipeHeater.Advanced", "AutoHeatPower", "Auto selects the best heat power to heat the gas (or liquid)", true);
+
+            PipeHeater_DesiredTemp = ConfigHandler.LoadFloat("PipeHeater.Advanced", "DesiredTemp", "Desired temperature (in celsius)", 20f);
         }
 
         public void Patch()
--- EfficientDevices/ModPatches.cs
+++ EfficientDevices/ModPatches.cs
@@ -15,7 +15,7 @@ namespace EfficientDevices
     public class ModPatches
     {
         static PowerRange Cable_VoltageRange = new PowerRange(Mod.Cable_MaxVoltage);
-        static PowerRange TurboPumpGas_PowerRange = new PowerRange(Mod.TurboPumpGas_MinPower, Mod.TurboPumpGas_MaxPower);
+        static PowerRange TurboPumpGas_PowerRange = new PowerRange(Mod.TurboPump_MinPower, Mod.TurboPump_MaxPower);
         static PowerRange AirConditioner_PowerRange = new PowerRange(Mod.AirConditioner_MinPower, Mod.AirConditioner_MaxPower);
 
         [HarmonyPatch(typeof(Cable), MethodType.Constructor)]
@@ -37,7 +37,7 @@ namespace EfficientDevices
 
         [HarmonyPatch(typeof(TurboVolumePump), nameof(TurboVolumePump.GetUsedPower))]
         [HarmonyPostfix]
-        static void TurboPumpGas_GetUsedPower_Postfix(ref float __result)
+        static void TurboPump_GetUsedPower_Postfix(ref float __result)
         {
             __result.AssignPower(TurboPumpGas_PowerRange);
         }
@@ -55,6 +55,82 @@ namespace EfficientDevices
             {
                 powerRequired.AssignPower(AirConditioner_PowerRange);
             }
+        }
+
+        [HarmonyPatch(typeof(PipeHeater), MethodType.Constructor)]
+        [HarmonyPostfix]
+        static void PipeHeater_Constructor(PipeHeater __instance)
+        {
+            //__instance.UsedPower = 0f;
+            __instance.HeatTransferJoulesPerTick = Mod.PipeHeater_HeatPower.Value;
+        }
+
+        [HarmonyPatch(typeof(PipeHeater), nameof(PipeHeater.OnAtmosphericTick))]
+        [HarmonyPrefix]
+        static bool PipeHeater_OnAtmosphericTick_Prefix(PipeHeater __instance, ref float ____powerUsedDuringTick)
+        {
+            // Remove heat dispersion
+            // base.OnAtmosphericTick();
+
+            var currentPa = __instance.NetworkAtmosphere.PressureGassesAndLiquidsInPa;
+            var currentTemp = __instance.NetworkAtmosphere.Temperature;
+
+            var advanced = Mod.PipeHeater_Advanced.Value;
+            var usedPower = Mod.PipeHeater_UsedPower.Value;
+            var heatPower = Mod.PipeHeater_HeatPower.Value;
+            var autoOnOff = Mod.PipeHeater_OnOffOnTemp.Value;
+            var autoHeatP = Mod.PipeHeater_AutoHeatPower.Value;
+            var desiredTemp = Mod.PipeHeater_DesiredTemp.Value + 273f;
+
+            if (advanced)
+            {
+                if (currentTemp <= desiredTemp)
+                {
+                    if (autoOnOff && !__instance.OnOff)
+                    {
+                        __instance.OnOff = true;
+                    }
+                    else if (!__instance.OnOff || !__instance.Powered)
+                    {
+                        return false;
+                    }
+
+                    if (__instance.NetworkAtmosphere == null || !__instance.NetworkAtmosphere.IsValid() || !__instance.HasOpenGrid)
+                    {
+                        return false;
+                    }
+
+                    __instance.NetworkAtmosphere.GasMixture.AddEnergy(autoHeatP ? currentPa / 100 : heatPower);
+                    __instance.UsedPower = usedPower;
+                    ____powerUsedDuringTick = usedPower;
+                }
+                else
+                {
+                    if (autoOnOff && __instance.OnOff)
+                    {
+                        __instance.OnOff = false;
+                    }
+                }
+
+                return false;
+            }
+
+            if (!__instance.OnOff || !__instance.Powered)
+            {
+                return false;
+            }
+            if (__instance.NetworkAtmosphere == null || !__instance.NetworkAtmosphere.IsValid() || !__instance.HasOpenGrid)
+            {
+                return false;
+            }
+            if (__instance.NetworkAtmosphere != null && __instance.NetworkAtmosphere.IsAboveArmstrong())
+            {
+                __instance.NetworkAtmosphere.GasMixture.AddEnergy(heatPower);
+                __instance.UsedPower = usedPower;
+                ____powerUsedDuringTick = usedPower;
+            }
+
+            return false;
         }
     }
 }
